acf5f50a4f5003ec5a3902b5b4034aa84853a3ba
Cooper deNicola <cdenicol@stanford.edu>
2019-11-06 01:18:07 -0800

    Flask recieves POST

--- server/app.py
+++ server/app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request
 import textprocessing
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def output():
-    return textprocessing.get_response("https://learn.adafruit.com/micropython-basics-loading-modules/import-code")
+    try:
+        if request.method == "GET":
+            return textprocessing.get_response("http://www.google.com")
+        if request.method == "POST":
+            return textprocessing.text_from_html(request.values.get('html'))
+    except Exception as e:
+        return flash(e)
 
 
 if __name__ == "__main__":
