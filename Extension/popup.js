7775096e1c21ae3446d020769b2b968259157ff8
Manuka Stratta <manukavalentina@hotmail.com>
2019-12-18 15:24:26 +0100

    Removing options file

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,15 +1,15 @@
-//Injects script into tab to extract HTML
-function scanForContent() {
-  const message = document.querySelector('#message');
-
-  chrome.tabs.executeScript(null, {
-    file: "content.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
-}
-
-window.onload = scanForContent;
+// //Injects script into tab to extract HTML
+// function scanForContent() {
+//   const message = document.querySelector('#message');
+//
+//   chrome.tabs.executeScript(null, {
+//     file: "content.js"
+//   }, function() {
+//     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+//     if (chrome.runtime.lastError) {
+//       message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+//     }
+//   });
+// }
+//
+// window.onload = scanForContent;
